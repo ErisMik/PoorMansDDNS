c7214dee40122dd1393376f99352660442b90ce7
Eric Mikulin <ericm99@gmail.com>
2022-03-08 00:04:17 -0800

    Initial bulk update of DNS records

--- src/main.rs
+++ src/main.rs
@@ -1,107 +1,167 @@
-extern crate clap;
-extern crate clokwerk;
-extern crate config;
-extern crate reqwest;
-extern crate retry;
-
-#[macro_use]
-extern crate serde_derive;
-
-use clap::{App, Arg};
+use clap::Parser;
 use clokwerk::{Scheduler, TimeUnits};
-use retry::{delay, retry};
+use config;
+use reqwest;
+use reqwest::header::{HeaderMap, HeaderValue};
+use serde::{Deserialize, Serialize};
+use serde_json;
 use std::collections::HashMap;
-use std::process::Command;
 use std::thread;
 use std::time::Duration;
 
 const HEALTHCHECK_HOST: &str = "https://hc-ping.com";
-
-#[derive(Clone, Debug, Deserialize)]
-struct DomainSettings {
-    domain: String,
-    healthcheck_uuid: String
-}
+const IPFY_URL: &str = "https://api.ipify.org";
+const CLOUDFLARE_HOST: &str = "https://api.cloudflare.com/client/v4";
 
 #[derive(Clone, Debug, Deserialize)]
 struct Settings {
-    domains: HashMap<String, DomainSettings>,
+    healthcheck_uuid: String,
+    dns_cloudflare_email: String,
+    dns_cloudflare_api_key: String,
 }
 
 impl Settings {
     fn new(config: config::Config) -> Result<Self, config::ConfigError> {
-        config.try_into()
+        config.try_deserialize()
     }
 }
 
-fn get_dns_ipaddr() {
+fn get_current_ipaddr() -> String {
+    let result = reqwest::blocking::get(IPFY_URL).unwrap();
+    return result.text().unwrap();
 }
 
-fn get_current_ipaddr() {
+#[derive(Clone, Debug, Deserialize)]
+struct CloudflareResponse<T> {
+    success: bool,
+    result: Vec<T>,
 }
 
-fn sync_domain(name: String, domain_settings: DomainSettings) {
-    let base_url = format!("{}/{}", HEALTHCHECK_HOST, domain_settings.healthcheck_uuid);
+#[derive(Clone, Debug, Deserialize)]
+struct CloudflareZonesResponse {
+    id: String,
+    name: String,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+struct CloudflareDNSListResponse {
+    id: String,
+    r#type: String,
+    name: String,
+    content: String,
+}
+
+#[derive(Clone, Debug, Serialize)]
+struct CloudflareDNSPatchRequest {
+    content: String,
+}
+
+fn cloudflare_get_request(
+    auth_key: &str,
+    auth_email: &str,
+    url: &str,
+) -> reqwest::blocking::Response {
+    let client = reqwest::blocking::Client::new();
+
+    let mut headers = HeaderMap::new();
+    headers.insert("X-Auth-Key", HeaderValue::from_str(auth_key).unwrap());
+    headers.insert("X-Auth-Email", HeaderValue::from_str(auth_email).unwrap());
+
+    return client.get(url).headers(headers).send().unwrap();
+}
+
+fn cloudflare_patch_request(
+    auth_key: &str,
+    auth_email: &str,
+    url: &str,
+    body: String,
+) -> reqwest::blocking::Response {
+    let client = reqwest::blocking::Client::new();
+
+    let mut headers = HeaderMap::new();
+    headers.insert("X-Auth-Key", HeaderValue::from_str(auth_key).unwrap());
+    headers.insert("X-Auth-Email", HeaderValue::from_str(auth_email).unwrap());
+
+    return client
+        .patch(url)
+        .headers(headers)
+        .body(body)
+        .send()
+        .unwrap();
+}
+
+fn update_dns_ipaddrs(auth_key: &str, auth_email: &str, new_ipv4: String) {
+    let zone_list_response =
+        cloudflare_get_request(auth_key, auth_email, &format!("{}/zones/", CLOUDFLARE_HOST))
+            .json::<CloudflareResponse<CloudflareZonesResponse>>()
+            .unwrap();
+
+    for zone in zone_list_response.result {
+        let record_list_response = cloudflare_get_request(
+            auth_key,
+            auth_email,
+            &format!("{}/zones/{}/dns_records", CLOUDFLARE_HOST, zone.id),
+        )
+        .json::<CloudflareResponse<CloudflareDNSListResponse>>()
+        .unwrap();
+
+        for dns_record in record_list_response.result {
+            match dns_record.r#type.as_str() {
+                "A" => {
+                    let payload = CloudflareDNSPatchRequest {
+                        content: new_ipv4.clone(),
+                    };
+                    cloudflare_patch_request(
+                        auth_key,
+                        auth_email,
+                        &format!(
+                            "{}/zones/{}/dns_records/{}",
+                            CLOUDFLARE_HOST, zone.id, dns_record.id
+                        ),
+                        serde_json::to_string(&payload).unwrap(),
+                    );
+                }
+                "AAAA" => {}
+                _ => {}
+            }
+        }
+    }
+}
+
+fn sync_domain(settings: Settings) {
+    let base_url = format!("{}/{}", HEALTHCHECK_HOST, settings.healthcheck_uuid);
     reqwest::blocking::get(&format!("{}/start", base_url)).unwrap();
 
-    let dns_ipaddr = get_current_domain_ipaddr();
     let current_ipaddr = get_current_ipaddr();
+    update_dns_ipaddrs(
+        &settings.dns_cloudflare_api_key,
+        &settings.dns_cloudflare_email,
+        current_ipaddr,
+    );
 
-    println!("Syncing {} with command {:?}", name, command);
-    let result = retry(delay::Fixed::from_millis(300000).take(5), || {
-        let mut child = command.spawn().expect("Command failed to start");
-        let result = child.wait().unwrap();
+    reqwest::blocking::get(&format!("{}/ok", base_url)).unwrap();
+}
 
-        let return_code = result.code().unwrap();
-        match return_code {
-            0 => Ok(return_code),
-            _ => Err(return_code),
-        }
-    });
-
-    match result {
-        Ok(return_code) => {
-            reqwest::blocking::get(&format!("{}/{}", base_url, return_code)).unwrap()
-        }
-        Err(error) => match error {
-            retry::Error::Operation { error, .. } => {
-                reqwest::blocking::get(&format!("{}/{}", base_url, error)).unwrap()
-            }
-            _ => reqwest::blocking::get(&format!("{}/{}", base_url, "fail")).unwrap(),
-        },
-    };
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Cli {
+    #[clap(short, long, default_value_t = String::from("pmddns.toml"))]
+    config: String,
 }
 
 fn main() {
-    let matches = App::new("Poorman's DDNS")
-        .arg(
-            Arg::with_name("config")
-                .short("c")
-                .long("config")
-                .value_name("FILE")
-                .help("Sets a custom config file")
-                .default_value("matt.toml")
-                .takes_value(true),
-        )
-        .get_matches();
+    let args = Cli::parse();
+    let builder = config::Config::builder().add_source(config::File::with_name(&args.config));
+    let config = builder.build().unwrap();
 
-    let config_filename = matches.value_of("config").unwrap();
-    let mut config = config::Config::new();
-    config
-        .merge(config::File::with_name(config_filename))
-        .unwrap();
     let settings = Settings::new(config).unwrap();
 
-    let mut scheduler = Scheduler::new();
+    sync_domain(settings.clone());
 
-    settings.domains.into_iter().for_each(|(name, domain_settings)| {
-        scheduler
-            .every(5.minute())
-            .run(|| sync_domain(name, domain_settings));
-    });
-
-    loop {
-        scheduler.run_pending();
-        thread::sleep(Duration::from_millis(1000));
-    }
+    // let mut scheduler = Scheduler::new();
+    // scheduler.every(5.minute()).run(move || sync_domain(settings.clone()));
+    // loop {
+    //     scheduler.run_pending();
+    //     thread::sleep(Duration::from_millis(1000));
+    // }
 }
